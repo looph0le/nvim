c96df8b20ddf62910193a6dfba4327fa541e85d9
looph0le <mitanshpanchal@gmail.com>
2025-07-03 02:34:23 +0530

    neovim dev setup

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+local servers = {"html", "cssls" , "typescript-language-server"}
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,7 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
+local o = vim.o
+o.cursorlineopt ='both' -- to enable cursorline!
+o.guifont = "SF Mono:h12" -- set the font for GUI applications
+vim.g.neovide_transparency = 0.8 -- set the opacity for Ne
