d7afdf22ab68e9b5a78a923cc28a9694651f4d4a
looph0le <mitanshpanchal@gmail.com>
2025-07-03 03:01:38 +0530

    Configure lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,12 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = {"html", "cssls" , "typescript-language-server"}
-vim.lsp.enable(servers)
+local servers = {"html", "cssls" , "ts_ls"}
 
+local lspconfig = require("lspconfig")
+for _, server in ipairs(servers) do
+  lspconfig[server].setup {
+    on_attach = require("nvchad.configs.lspconfig").on_attach,
+    capabilities = require("nvchad.configs.lspconfig").capabilities,
+  }
+end
 -- read :h vim.lsp.config for changing options of lsp servers 
