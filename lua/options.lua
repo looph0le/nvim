2cdf07663deff2c459ea1b001a09f9363c6809f9
looph0le <mitanshpanchal@gmail.com>
2025-07-03 04:17:43 +0530

    neovide config

--- lua/options.lua
+++ lua/options.lua
@@ -3,6 +3,12 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
+local g = vim.g
+
 o.cursorlineopt ='both' -- to enable cursorline!
+o.relativenumber = true -- enable relative line numbers
+
+-- neovide config
 o.guifont = "SF Mono:h12" -- set the font for GUI applications
-vim.g.neovide_transparency = 0.8 -- set the opacity for Ne
+g.neovide_transparency = 0.9 -- set the opacity for Neovide
+g.neovide_scale_factor = 1.0  -- default zoom level
